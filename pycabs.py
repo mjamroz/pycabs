312b2ad914d2b828c99d33c757aa7292cb455ba1
Michal Jamroz <michal@yerbamate.com.pl>
2012-06-29 15:47:01 +0200

    Zaczal juz dzialac.

--- pycabs.py
+++ pycabs.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from tempfile import mkdtemp
-from re import search
+from re import search,sub
 from os import getcwd,chdir
 from subprocess import Popen,PIPE
-
+from shutil import copyfile
+from time import clock
 
 class CABS:
 	"""CABS main class.
@@ -16,19 +17,71 @@ class CABS:
 	:type secondary_structure: string
 	:param templates_filenames: path to 3D protein model templates in pdb file format which you want to use for modeling. Cα numbering in templates must be aligned to target sequence
 	:type templates_filenames: list
+	:param project_name: files for that particular modeling will be named with prefix project_name.
+	:type project_name: string
+	
+	.. warning:: it will overwrite previous SEQ file.
 	"""
 
-	def __init__(self,sequence,secondary_structure,templates_filenames):
+	def __init__(self,sequence,secondary_structure,templates_filenames,project_name="fnord"):
 		if len(sequence)!=len(secondary_structure):
-			raise Errors("Different lenght of sequence and secondary structure")
+			raise Errors("Different lengths of sequence and secondary structure")
 		self.sequence = sequence
+		sub(r'\s', '', project_name)
+		self.name = project_name
 		self.ss = secondary_structure
 		self.templates_fn = templates_filenames
 		self.seqlen = len(sequence)
 		
+		self.constraints = 0
+		# self.replicas - defined in lattie model creation method
 		self._createSEQ() # we always need SEQ file so I put it here
 
+	def calcConstraints(self,exclude_residues=[]): # TODO - update self.constraints
+		pass
+	def trafToPdb(self, output_filename = "TRAF.pdb"):
+		pass	
+	def _copyFFFiles(self):
+		path_to_ff_dir="/home/hydek/pycabs/FF" # TODO
+		for fff in ["R13","R13A","CENTRO","QUASI3S","R13C","R13E",\
+		"R13H","R14","R14A","R14C","R14E","R14H","R15","R15A","R15C",\
+		"R15E","R15H","SIDECENT"]:
+			copyfile(path_to_ff_dir+"/"+fff,"./"+fff)
+			
+	def copyChains(self):
+		copyfile("ACHAINS_NEW","FCHAINS")
+		
+	def modeling(self, Ltemp=1.0,Htemp=2.0,cycles=20,phot=10,constraints_force=1.0):
+		self._copyFFFiles()
+		
+		# create INP file
+		rng_seed = 1799
+		inp = "%d %d %d %d\n%5.2f %5.2f %5.2f %5.3f %5.3f\n%5.2f %5.2f"\
+		 "%5.2f %5.2f %5.3f\n0. 0 0 0 0\n0.0 0.0 0.0 0.0\n0 0.0\n%6d"\
+		 "%5.2f" %(rng_seed,cycles,phot,self.replicas,Htemp,Ltemp,4.0,\
+		 0.125,0.250,1.0,2.0,0.25,-2.00,0.375,self.constraints,constraints_force)
+		try:
+			f = open("INP","w")
+			f.write(inp)
+			f.close()
+		except IOError as e:
+			print "I/O error({0}): {1}".format(e.errno, e.strerror)
+		print Info("INP file created")	
+		# run CABS	
+		# TODO zmodyfikowac CABS odnosnie OUT i TRAF
+		print Info("CABS modeling software...")
+		path_to_cabs="/home/hydek/pycabs/FF/" # TODO
+		arg = path_to_cabs+"cabs" 
+		cabs_start = clock()
 
+		cabsstart = Popen([arg], shell=True, stdout=PIPE)
+		cabsstart.communicate()
+
+		elapsed = (clock() - cabs_start)/60
+
+		print Info("Done. Running time: %6d min." %(elapsed))
+		
+		
 	def _createSEQ(self):
 		"""
 			Create SEQ input file for CABS, which contains sequence and (predicted) secondary structure of target chain.
@@ -36,9 +89,8 @@ class CABS:
 			SEQ file is three-column file, where first column is index of residue (1...N), 
 			second is three letter aminoacid, and third is secondary structure (1: coil, 2: helix, 4: beta)
 			
-			SEQ is required, and has to be created each time so we added it to __init__ of this class. 
-			.. warning::
-			It overrides any SEQ in working directory.
+			SEQ is required, and has to be created each time so we added it to __init__ of CABS class. 
+			**WARNING**: It overwrites any SEQ in working directory.
 		"""
 		seq_trans={'A': "ALA",'R': "ARG",'N':"ASN",'D':"ASP",'C':"CYS",\
 		'E':"GLU",'Q':"GLN",'G':"GLY",'H':"HIS",'I':"ILE",'L':"LEU",\
@@ -73,14 +125,18 @@ class CABS:
 			
 			:param start_structures_fn: list of paths to pdb files which should be used instead of templates models.  This parameter is optional, and probably not often used. Without it script creates replicas from templates files.
 			:type start_structures_fn: list
-			:param replicas: define number of replicas in CABS simulation. Usually you don't need to change it, default 20 is optimal for most cases. If number of replicas is smaller than number of templates - program will create replicas from first R templates. If there is less templates than replicas, replicas are generated sequentially from template models.
+			:param replicas: define number of replicas in CABS simulation. However 20 is optimal for most cases, and you don't need to change it in protein modeling case. 
 			:type replicas: integer
+			
+			.. note:: If number of replicas is smaller than number of templates - program will create replicas using first *replicas* templates. If there is less templates than replicas, they are creating sequentially using template models.
+			
+			.. warning:: it will overwrite FCHAINS file from working directory
 		"""
 		if len(start_structures_fn)==0 and len(self.templates_fn)==0:
 			raise Errors("lists start_structures_fn OR templates_filenames cannot be empty !")
 			
-		tempdir = mkdtemp('','CABStmp_','.')
-		
+		tempdir = mkdtemp('',self.name+'_tmp','.')
+		self.tempdir = tempdir
 		temp_filenames = self.templates_fn
 		if len(start_structures_fn)>0:	# for user selected start models
 			temp_filenames = start_structures_fn
@@ -103,7 +159,7 @@ class CABS:
 				fw.close()		
 				f.close()
 				chdir(tempdir)
-				arg = "../FF/a.out %d %d %d" % (self.seqlen,l,i) # TODO
+				arg = "/home/hydek/pycabs/FF/a.out %d %d %d" % (self.seqlen,l,i) # TODO
 				chainstart = Popen([arg], shell=True, stdout=PIPE)
 				chainstart.communicate()
 			
@@ -121,7 +177,6 @@ class CABS:
 			chains_data.append(chain.readlines())
 			chain.close()
 			
-	
 		counter = 0
 		k = 0 					# index of template
 		fchains = open("FCHAINS","w")				
@@ -132,11 +187,93 @@ class CABS:
 			if k==m: k=0
 			counter +=1
 		fchains.close()	
+		self.replicas = replicas
 		print Info("FCHAINS file created")
 			
 
 				
 			
+
+
+#33############ Utilities ########################
+def parsePsipredOutput(psipred_output_fn):
+	"""
+	Psipred (protein secondary structure prediction, http://bioinf.cs.ucl.ac.uk/psipred/) output parser. 
+	Psipred output looks like: ::
+	
+			> head psipred.ss
+			1 P C   1.000  0.000  0.000
+			2 K C   0.665  0.000  0.459
+			3 A E   0.018  0.000  0.991
+			4 L E   0.008  0.000  0.997
+			5 I E   0.002  0.000  0.998
+			6 V E   0.003  0.000  0.999
+			7 Y E   0.033  0.000  0.981
+
+	:param psipred_output_fn: path to the psipred output file
+	:type psipred_output_fn: string
+	:returns: tuple (sequence, secondary_structure)
+		
+	"""
+	coil = ["-","T","S","B","G","I"]
+	seq = ""
+	ss = ""
+	try:
+		f = open(psipred_output_fn)
+		for line in f.readlines():
+			d = line.split()
+			if d[2] in coil: d[2] = "C"
+			seq = seq + d[1]
+			ss = ss + d[2]
+		f.close()
+	except IOError as e:
+		print "I/O error({0}): {1}".format(e.errno, e.strerror)
+	return (seq,ss)
+	
+def parsePorterOutput(porter_output_fn):
+	"""
+		Porter (protein secondary stucture prediction, http://distill.ucd.ie/porter/) output parser. 
+		Porter output looks like: ::
+		
+			IDVLLGADDGSLAFVPSEFSISPGEKIVFKNNAGFPHNIVFDEDSIPSGVDASKISMSEE
+			CEEEECCCCCCCCEECCEEEECCCCEEEEEECCCCCEEEEECCCCCCCCCCHHHHCCCCC
+
+
+
+			DLLNAKGETFEVALSNKGEYSFYCSPHQGAGMVGKVTVN
+			CCECCCCCEEEEECCCCEEEEEECCHHHHCCCEEEEEEC
+
+		.. note:: Use only sequence/secondary structure fragment of Porter email
+		
+		:param porter_output_fn: path to the porter output file
+		:type porter_output_fn: string
+		:returns: tuple (sequence, secondary_structure)
+		
+	"""
+	coil = ["-","T","S","B","G","I"]
+	seq = ""
+	ss = ""
+	try:
+		f = open(porter_output_fn)
+		i=0
+		for line in f.readlines():
+			line = line.strip()
+			if line=="":
+				continue
+			if (i%2)==0:
+				seq = seq+line
+			else:
+				ss = ss+line	
+			i += 1
+		f.close()
+		# replace other than HCE into C
+		for c in coil: ss = ss.replace(c,'C')
+	except IOError as e:
+		print "I/O error({0}): {1}".format(e.errno, e.strerror)
+	return (seq,ss)
+	
+
+
 
 class Info():
 	"""
@@ -158,12 +295,17 @@ class Errors(Exception):
 		return repr(self.value)
 
 
+#################################################################
+
+
+
 
 # tests
 if __name__ == "__main__":
-	seq = "IDVLLGADDGSLAFVPSEFSISPGEKIVFKNNAGFPHNIVFDEDSIPSGVDASKISMSEEDLLNAKGETFEVALSNKGEYSFYCSPHQGAGMVGKVTVN"
-	ss =  "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCEEEEEEEEEEEEEEEEEEEEEEEEEE"
-	templates = ["playground/2pcy_CA.pdb","playground/2pcy.pdb"]
-	a = CABS(seq,ss,templates)
+	data =  parsePorterOutput("playground/porter.ss")
 
+	templates = ["playground/2pcy_CA.pdb"]
+	a = CABS(data[0],data[1],templates)
 	a.createLatticeReplicas()
+	a.modeling()
+#	print parsePsipredOutput("playground/psipred.ss")
