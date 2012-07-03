c7010c25b4b8dbe82e25cc1684b389e0a6bc6af6
Michal Jamroz <michal@yerbamate.com.pl>
2012-07-03 14:09:19 +0200

    heat capacity

--- examples/heat_capacity.py
+++ examples/heat_capacity.py
@@ -2,20 +2,31 @@
 
 import pycabs
 import multiprocessing as mp
-
-def runCABS(temperature,name,templates,sequence,ss):
+import os
+def runCABS(temperature):
+	global name, sequence,secstr,template
+	
 	# function for running CABS with different temperatures
 	# it will compute in directory name+_+temperature
-	a = CABS(sequence,ss,templates,name+"_"+str(temperature))
+	here = os.getcwd()
+	a = pycabs.CABS(sequence,secstr,template,name+"_"+str(temperature))
 	a.createLatticeReplicas()
 	a.modeling(Ltemp=temperature,Htemp=temperature, phot=1,cycles=1)
+	os.chdir(here)
 
 
 # lets create thread pool 
 
 name = "fnord"
-temperatures=range(1,10)
+template=["/home/hydek/pycabs/playground/2pcy_CA.pdb"]
+sss =  pycabs.parsePorterOutput("/home/hydek/pycabs/proba/playground/porter.ss") # suppose we have porter prediction of sec. str.
+sequence = sss[0]
+secstr = sss[1]
+
+# creating working pool 
 pool = mp.Pool(processes=2)
+temperatures=xrange(1,5) # ranges of temperature
 
+pool.map(runCABS,temperatures)
 
-# wait ...
+# wait ... and process name*/ENERGY files
