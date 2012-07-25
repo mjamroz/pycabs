2a78ae5aa0eef3c9a478d029ae23f5c3a61c120c
Michal Jamroz <michal@yerbamate.com.pl>
2012-07-25 16:52:33 +0200

    merge pycabs and template *py

--- pycabs.py
+++ pycabs.py
@@ -19,14 +19,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 from tempfile import mkdtemp
-from re import search,sub
+from re import search,sub,compile,match
 from os import getcwd,chdir,path,mkdir,stat,remove,rename
-from numpy import fromfile
+from numpy import fromfile,reshape,linalg
 from subprocess import Popen,PIPE
 from shutil import copyfile
 from math import sqrt,cos,sin,atan2
 import threading
 import time
+
 
 class CABS(threading.Thread):
 	"""
@@ -668,6 +669,53 @@ class Errors(Exception):
 	def __str__(self):
 		return repr(self.value)
 
+class Template:
+	
+	"""
+		Class used for template storage of atom positions and distance calculation
+		
+		:param filename: path to file with template (in PDB format)
+		
+	"""
+
+	def __init__(self,filename):
+		
+		self.hashmap={}
+		atm = compile(r"^ATOM.{9}CA.{7}(?P<resid>.{4})(?P<x>.{12})(?P<y>.{8})(?P<z>.{8})")
+		f = open(filename)
+		coordinateslist = []
+		i=0
+		for line in f.readlines():
+			data = atm.match(line) # parse pdb file
+			if data:
+				self.hashmap[int(data.group('resid'))] = i # remember residues indexes
+				i+=1
+				for v in data.groups()[1:]: coordinateslist.append(float(v))
+		f.close()
+		self.coordinates = reshape(coordinateslist,(-1,3)) # magic, reshape list of xyzxyz into Nx3 array
+		coordinateslist = []
+		self.residues = self.hashmap.keys()
+	def printCoordinates(self):	
+		for i in self.hashmap.keys():
+			print i,self.hashmap[i],self.coordinates[self.hashmap[i]]
+	def getCoordinate(self,residue_index):
+		idx = int(residue_index)
+		if idx in self.residues:
+			return self.coordinates[idx]
+	def distance(self,idx_i,idx_j):
+		"""
+			:param idx_i: residue index (as in target sequence numbering)
+			:param idx_j: residue index (as in target sequence numbering)
+			:type idx_i: integer
+			:type idx_j: integer
+			:return: euclidean distance between Cα(i) and Cα(j)
+		"""
+		i = int(idx_i)
+		j = int(idx_j)
+		if i in self.residues and j in self.residues:
+			vi = self.coordinates[i]
+			vj = self.coordinates[j]
+			return linalg.norm(vi-vj)
 
 		
 #################################################################
