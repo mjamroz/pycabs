a4f041be8596b2f4159c3c3087d46482749b2ab2
Michal Jamroz <michal@yerbamate.com.pl>
2012-07-25 16:21:39 +0200

    parsed pdb

--- template.py
+++ template.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python
 from re import compile,match
-
+from numpy import reshape
 class Template:
+	
 	"""
 		Class used for template storage and atoms distance calculation
 	"""
 
 	def __init__(self,filename):
+		
+		self.hashmap={}
 		atm = compile(r"^ATOM.{9}CA.{7}(?P<resid>.{4})(?P<x>.{12})(?P<y>.{8})(?P<z>.{8})")
 		f = open(filename)
+		coordinateslist = []
+		i=0
 		for line in f.readlines():
 			data = atm.match(line) # parse pdb file
 			if data:
-				print data.groupdict(),data.group('resid')
+				self.hashmap[int(data.group('resid'))] = i # remember residues indexes
+				i+=1
+				for v in data.groups()[1:]: coordinateslist.append(float(v))
 		f.close()
+		self.coordinates = reshape(coordinateslist,(-1,3)) # magic, reshape list of xyzxyz into Nx3 array
+		coordinateslist = []
+	def printCoordinates(self):	
+		for i in self.hashmap.keys():
+			print i,self.hashmap[i],self.coordinates[self.hashmap[i]]
+
 
 
 
 
 a = Template("./playground/2pcy.pdb")
+a.printCoordinates()
